bca4af685b26d3f614f2f2212e611a71fef4311e
hasecom <hasetennisth@gmail.com>
2020-05-28 19:06:57 +0900

    update

--- slander/src/main.js
+++ slander/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas,fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 Vue.config.productionTip = false
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+const router = new VueRouter({ 
+
+});
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
